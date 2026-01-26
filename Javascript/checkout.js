e902ca38fee835b54baec93734d7754d46ba6725
ThongWan1706 <s10275182@connect.np.edu.sg>
2026-01-26 10:02:55 +0800

    Add toString to the ShippingCost, gst and total as the data type in the restdb is text

--- Javascript/checkout.js
+++ Javascript/checkout.js
@@ -129,9 +129,9 @@ async function handleCheckoutSubmit(e) {
       "Email": document.getElementById('custEmail')?.value || "",
       "DeliveryAddress": isShipping ? (document.getElementById('address')?.value || "") : "Self-Pickup",
       "ShippingRequired": isShipping,
-      "ShippingCost": shippingCost,
-      "GST": gst,
-      "Total": finalTotal,
+      "ShippingCost": shippingCost.toString(),
+      "GST": gst.toString(),
+      "Total": finalTotal.toString(),
       "OrderStatus": "Pending"
     };
 
